33014994c9c9a1af82f602329e760c2e4795a8e6
Ryan-Balachandran <ryanbalachandran@yahoo.com>
2021-03-26 14:59:04 -0400

    Update and rename colors.py to constants.py
    added constant variables from GameBoard.py

---
+++ constants.py
@@ -0,0 +1,15 @@
+# Define some colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+BLUE = (55, 55, 255)
+
+# This sets the WIDTH and HEIGHT of each grid location
+width, height = 60, 60
+
+# This sets the margin between each tile
+margin = 4
+
+# Set the Width and Height of the screen [width, height]
+size = [(width * 10) + (margin * 10), (height * 10) + (margin * 10)]
