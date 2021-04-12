ea4ada3077bd4bb1436b22cce16c90cb211aaadc
Matt Bonanno <4009945+MattBDev@users.noreply.github.com>
2021-04-12 12:09:23 -0400

    Added a direction and Orientation enum

---
+++ direction.py
@@ -0,0 +1,10 @@
+from enum import Enum
+class Direction(Enum):
+    NORTH = 1
+    SOUTH = 2
+    EAST = 3
+    WEST = 4
+
+class Orientation(Enum):
+    HORIZONTAL = 1  # When the ship is along the x axis.
+    VERTICAL = 0  # when the ship is along the y axis. 
